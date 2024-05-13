d9a3f4c2e45f05bc9c8a37b364831c327907248f
Jose <josevargaspoma@gmail.com>
2024-05-12 23:33:43 -0400

    Consultas Avanzadas

--- rutas/adopcionesRutas.js
+++ rutas/adopcionesRutas.js
@@ -53,4 +53,51 @@ rutas.delete('/eliminar/:id',async (req, res) => {
         res.status(500).json({ mensaje :  error.message})
     }
 });
+//5. endpoint: Obtener adopciones por género y rango de edad
+rutas.get('/filtrarPorGeneroEdad/:genero/:min/:max', async (req, res) => {
+    try {
+        const { genero, min, max } = req.params;
+        const adopciones = await AdopcionesModel.find({ genero, edad: { $gte: min, $lte: max } });
+        res.json(adopciones);
+    } catch (error) {
+        res.status(500).json({ mensaje: error.message });
+    }
+});
+//6. endpoint: Obtener estadísticas de adopciones por género de mascota
+rutas.get('/estadisticasPorGenero', async (req, res) => {
+    try {
+        const estadisticas = await AdopcionesModel.aggregate([
+            { $group: { _id: '$genero', total: { $sum: 1 } } },
+            { $project: { _id: 0, genero: '$_id', total: 1 } }
+        ]);
+        res.json(estadisticas);
+    } catch (error) {
+        res.status(500).json({ mensaje: error.message });
+    }
+});
+//7. endpoint: Eliminar adopciones por rango de edad
+rutas.delete('/eliminarPorRangoEdad/:min/:max', async (req, res) => {
+    try {
+        const { min, max } = req.params;
+        const adopcionesEliminadas = await AdopcionesModel.deleteMany({ edad: { $gte: min, $lte: max } });
+
+        if (adopcionesEliminadas.deletedCount === 0) {
+            return res.status(404).json({ mensaje: 'No se encontró ninguna adopción en este rango de edad.' });
+        }
+
+        return res.json({ mensaje: 'Se eliminaron los registros de adopción dentro del rango de edad especificado.' });
+    } catch (error) {
+        res.status(500).json({ mensaje: error.message });
+    }
+});
+//8. endpoint: Obtener adopciones por la fecha de vacunacion
+rutas.get('/FechaVacunacionEspecifica/:fecha', async (req, res) => {
+    try {
+        const { fecha } = req.params;
+        const adopciones = await AdopcionesModel.find({ fecha_vacunas: fecha });
+        res.json(adopciones);
+    } catch (error) {
+        res.status(500).json({ mensaje: error.message });
+    }
+});
 module.exports = rutas;
